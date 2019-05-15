16bc9e9e61e29729120f3fa0ea6f26a3612a5789
kayky lopes <kaykylopes@hotmail.com>
2019-05-15 12:18:42 -0300

    CRUD Acaffolding

---
+++ ProjetoMvc/Data/ProjetoMvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoMvc.Models
+{
+    public class ProjetoMvcContext : DbContext
+    {
+        public ProjetoMvcContext (DbContextOptions<ProjetoMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoMvc.Models.Department> Department { get; set; }
+    }
+}
--- ProjetoMvc/Startup.cs
+++ ProjetoMvc/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ProjetoMvc.Models;
 
 namespace ProjetoMvc
 {
@@ -33,6 +35,9 @@ namespace ProjetoMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ProjetoMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProjetoMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
